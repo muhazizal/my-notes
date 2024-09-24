717853c6528c6d6a07b230b7beb2eb5b9c12944e
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2024-09-24 13:25:55 +0700

    feat: add user server plugin

---
+++ plugins/user.server.ts
@@ -0,0 +1,11 @@
+import { useUserStore } from '@/stores/user'
+import type { Pinia } from 'pinia'
+
+export default defineNuxtPlugin(async ({ $pinia }): Promise<void> => {
+	const access_token = useCookie('access_token')
+	const { getUserProfile } = useUserStore($pinia as Pinia)
+
+	if (access_token.value) {
+		await getUserProfile()
+	}
+})
