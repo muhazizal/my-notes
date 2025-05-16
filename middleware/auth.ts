a5e080a515d31c89e196748b43b343ce81c75b45
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-05-16 14:48:37 +0700

    fix: delete middleware

--- middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-	const access_token = useCookie('access_token')
-
-	if (!access_token.value) {
-		const router = useRouter()
-		router.replace('/sign-in')
-	}
-})
