578197a14e432fb33c5185dac6b0cd109b192d56
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-06-20 14:46:11 +0700

    fix(note): hover up note

--- components/Notes/Item.vue
+++ components/Notes/Item.vue
@@ -8,8 +8,8 @@
 
 <style lang="scss" scoped>
 .item {
-	@apply w-48 h-48 border rounded-lg flex justify-center items-center text-slate-600 transition-all;
-	@apply hover:cursor-pointer hover:bg-slate-50;
+	@apply w-48 h-48 border rounded-lg flex justify-center items-center text-slate-600 transition-all transform;
+	@apply hover:cursor-pointer hover:bg-slate-50 hover:-translate-y-1;
 	@apply active:scale-95 active:bg-slate-100;
 }
 </style>
