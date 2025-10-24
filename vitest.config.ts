e7939e1f52ef45ade0b84532462901c97974525b
Muhamad Aziz Al-asaad <muhazizal.26@gmail.com>
2025-10-25 00:15:18 +0700

    feat: enable vitest treshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./', import.meta.url)),
@@ -27,6 +27,12 @@ export default defineConfig({
 				'server/**/*',
 			],
 			exclude: ['tests/**/*', '**/*.d.ts', '**/*.test.*', '**/*.spec.*', 'node_modules/**/*'],
+			thresholds: {
+				lines: 90,
+				statements: 90,
+				functions: 90,
+				branches: 95,
+			},
 		},
 	},
 })
